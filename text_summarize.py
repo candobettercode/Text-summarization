084c0ce2f0c154a524ea66527186bd829b006433
Siddhesh Masurkar <Siddhesh.Masurkar@sbmp.ac.in>
2023-08-17 12:05:57 +0530

    update text_summarize.py

--- text_summarize.py
+++ text_summarize.py
@@ -2,6 +2,7 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation 
 from heapq import nlargest
+import en_core_web_sm
 
 def summarizer(rawdocs):
     stopwords = list(STOP_WORDS)
